48419fb2a60eabf52e49d3a459c98f5c636e58f7
Erinad-2002 <Erinad.business@gmail.com>
2021-10-29 13:06:56 +0200

    tulajdonos class

---
+++ src/com/company/Tulajdonos.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public final class Tulajdonos {
+
+    private String nev;
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+}
