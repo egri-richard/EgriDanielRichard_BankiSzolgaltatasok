029192ddfa81d170d37bf961ba083575189dba0e
Erinad-2002 <Erinad.business@gmail.com>
2021-10-29 13:51:31 +0200

    kartya done

--- src/com/company/Kartya.java
+++ src/com/company/Kartya.java
@@ -1,4 +1,27 @@
 package com.company;
 
-public class Kartya {
+public class Kartya extends BankiSzolgaltatas{
+
+    private Szamla szamla;
+    private String kartyaSzam;
+
+    public Kartya(Tulajdonos tulajdonos, Szamla szamla, String kartyaSzam) {
+        super(tulajdonos);
+        this.szamla = szamla;
+        this.kartyaSzam = kartyaSzam;
+    }
+
+    public String getKartyaSzam() {
+        return kartyaSzam;
+    }
+
+    public boolean vasarlas(int osszeg) {
+        boolean success = false;
+
+        if (szamla.kivesz(osszeg)) {
+            success = true;
+        }
+
+        return success;
+    }
 }
